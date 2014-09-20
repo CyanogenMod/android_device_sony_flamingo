9d0b7991786fea6036e9d5afe09105598ce0dd35
Adam Farden <adam@farden.cz>
2014-09-20 19:05:16 +0200

    Add glove mode

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -41,6 +41,9 @@ TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
 COMMON_GLOBAL_CFLAGS += -D__ARM_USE_PLD -D__ARM_CACHE_LINE_SIZE=64
 
+# Hardware Features
+BOARD_HARDWARE_CLASS := device/sony/tianchi/cmhw
+
 # Kernel information
 BOARD_KERNEL_BASE     := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
