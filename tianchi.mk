0c6f3934295f02b3ad6d1385fb90e3c9416fbb68
Adam Farden <adam@farden.cz>
2014-10-02 17:48:43 +0200

    Update config files

--- tianchi.mk
+++ tianchi.mk
@@ -165,12 +165,6 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/rootdir/system/etc/sensor_def_qcomdev.conf:system/etc/sensor_def_qcomdev.conf \
-    $(COMMON_PATH)/rootdir/system/etc/sensor_def_somc.conf:system/etc/sensor_def_somc.conf \
-    $(COMMON_PATH)/rootdir/system/etc/sensors_calib.conf:system/etc/sensors_calib.conf \
-
-# System Monitor (Thermal Control)
-PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/rootdir/system/etc/sysmon.cfg:system/etc/sysmon.cfg
 
 # USB
 PRODUCT_PACKAGES += \
