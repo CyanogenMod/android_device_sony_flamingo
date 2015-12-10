e15bc0dd7491349422a27a4b15e38e7c56f50d7b
Adam Farden <adam@farden.cz>
2015-12-10 22:57:50 +0100

    Initial CM Bringup
    Change-Id: I37fc770023428aa5adddb62333f327601bda8a6c

---
+++ cm.mk
@@ -0,0 +1,23 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 854
+TARGET_SCREEN_WIDTH := 480
+
+$(call inherit-product, device/sony/flamingo/aosp_d2203.mk)
+
+# Inherit CM common Phone stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+# Recovery
+PRODUCT_COPY_FILES += \
+    device/sony/flamingo/bootrec-device:root/sbin/bootrec-device
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=D2203
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/D2203/D2203:4.4.2/18.4.C.1.29/4nv_bQ:user/release-keys
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="D2203-user 4.4.2 18.4.C.1.29 4nv_bQ release-keys"
+
+PRODUCT_NAME := cm_flamingo
+PRODUCT_DEVICE := flamingo
+PRODUCT_MODEL := Xperia E3
