dd327334bb7b789ee142f244f4538e39704e1f85
Adam Farden <adam@farden.cz>
2016-01-23 20:07:39 +0100

    flamingo: Use the source-built Sony init
    Change-Id: I11aaaa44082c860ef151c319f44c3827b9156eb2
    Signed-off-by: Adam Farden <adam@farden.cz>

--- cm.mk
+++ cm.mk
@@ -10,10 +10,6 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
-# Recovery
-PRODUCT_COPY_FILES += \
-    device/sony/flamingo/bootrec-device:root/sbin/bootrec-device
-
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=D2203
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/D2203/D2203:4.4.2/18.4.C.1.29/4nv_bQ:user/release-keys
 PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="D2203-user 4.4.2 18.4.C.1.29 4nv_bQ release-keys"
