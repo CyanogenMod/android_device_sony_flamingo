a1760cad21e80ec2ae978fd10329f00d8ee7a37d
galaxyfreak <marinspajic17@hotmail.com>
2016-01-27 17:45:27 +0100

    Switch PRODUCT_NAME and PRODUCT_MODEL
    For "unified tree"
    
    Change-Id: I11c085bf9b5e677dd4984366f601bf153f9a4fb4

--- cm.mk
+++ cm.mk
@@ -10,10 +10,10 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=D2203
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=Xperia E3
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/D2203/D2203:4.4.2/18.4.C.1.29/4nv_bQ:user/release-keys
 PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="D2203-user 4.4.2 18.4.C.1.29 4nv_bQ release-keys"
 
 PRODUCT_NAME := cm_flamingo
 PRODUCT_DEVICE := flamingo
-PRODUCT_MODEL := Xperia E3
+PRODUCT_MODEL := D2203
